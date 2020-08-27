7e0440237297371ed2432ce8d3a54b181e02e450
João <joaopfernandesc@gmail.com>
2020-08-27 18:10:24 -0300

    Feito controller de médicos

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def update
+    result = UpdateDoctor.call(doctor_params: doctor_params, id: params[:id])
+
+    if result.success?
+      redirect_to root_path, notice: 'Médico editado com sucesso!'
+    else
+      redirect_to :doctor, alert: 'Já existe um médico cadastrado com este CRM.'
+    end
+  end
+
+  def create
+    result = CreateDoctor.call(doctor_params: doctor_params)
+
+    if result.success?
+      redirect_to root_path, notice: 'Médico criado com sucesso!'
+    else
+      redirect_to new_doctor_path, alert: 'Já existe um médico cadastrado com este CRM.'
+    end
+  end
+
+  def destroy
+    result = DeleteDoctor.call(id: params[:id])
+
+    if result.success?
+      redirect_to root_path, notice: 'Médico removido com sucesso!'
+    else
+      redirect_to root_path, alert: result.error
+    end
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :crm, :crm_uf)
+  end
+end
